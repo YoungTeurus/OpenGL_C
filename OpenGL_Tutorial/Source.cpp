9bc63e2d879803d5167876927169be7c44771e03
Little_Teu <s_azarenko@outlook.com>
2021-03-09 13:12:34 +0300

    Матрицы модели, вида и проекции

--- OpenGL_Tutorial/Source.cpp
+++ OpenGL_Tutorial/Source.cpp
@@ -44,6 +44,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	int windowWidth = 500, windowHeight = 500;
 
 	GLFWwindow* win = glfwCreateWindow(500, 500, "OpenGL Window",
 		NULL, NULL);
@@ -68,21 +69,68 @@ int main() {
 	// Конец настройки glfw
 
 	// Исходные данные:
-	const int numOfVertexes = 4, sizeOfVertex = 9;
+	const int numOfVertexes = 36, sizeOfVertex = 9;
 
 	float polygon[numOfVertexes * sizeOfVertex] = {
 		//   X       Y       Z           R       G       B       A		     S       T
-			0.5f,	0.0f,   0.0f,		1.0f,	0.0f,	0.0f,	1.0f,		1.0f,	0.0f,
-			0.0f,	0.5f,	0.0f,		0.0f,	1.0f,	0.0f,	1.0f,		0.5f,	1.0f,
-		   -0.5f,	0.0f,	0.0f,		0.0f,	0.0f,	1.0f,	1.0f,		0.0f,	0.0f,
-		    0.0f,  -0.5f,	0.0f,		0.0f,	1.0f,	0.0f,	1.0f,		0.5f,	1.0f,
+		    -0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+		
+			-0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+		
+			-0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+		
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+		
+			-0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+		
+			-0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
 	};
 
-	const int numOfIndices = 2;
+	const int numOfIndices = 12;
 
 	unsigned indices[numOfIndices * 3] = {
 		0, 1, 2,
-		0, 2, 3,
+		1, 2, 3,
+		0, 2, 4,
+		2, 4, 6,
+		0, 4, 1,
+		4, 1, 5,
+		1, 3, 5,
+		3, 5, 7,
+		2, 3, 6,
+		3, 6, 7,
+		4, 5, 6,
+		5, 6, 7
 	};
 
 	// Конец исходных данных
@@ -118,6 +166,8 @@ int main() {
 	// Сбрасываем VAO:
 	glBindVertexArray(0);
 
+	glEnable(GL_DEPTH_TEST);
+
 	Shader *basicShader = new Shader("./shaders/basic.vert", "./shaders/basic.frag");
 	Texture* containerTexture = new Texture("./textures/container.jpg");
 	Texture* pogfaceTexture = new Texture("./textures/awesomeface.png", TextureType::RGBA);
@@ -131,27 +181,39 @@ int main() {
 	basicShader->setInt("uTexture", 0);  // GL_TEXTURE0
 	basicShader->setInt("uTexture2", 1); // GL_TEXTURE1
 
-	// Трансформации:
-	glm::mat4 trans = glm::mat4(1.0f);
 
-	// "Правильный" порядок: перемещение, поворот, изменение размеров
-	trans = glm::translate(trans, glm::vec3(0.5f, 0.5f, 0.5f));  // Передвинуть объект на вектор (0.5, 0.5, 0.5)
-	trans = glm::rotate(trans, glm::radians(30.0f), glm::vec3(0.0f, 0.0f, 1.0f));  // Повернуть объект относительно вектора (0, 0, 1) на 30 градусов
-	trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));  // Увеличить измерения объекта в (0.5, 0.5, 0.5)
+	// Model matrix: размещение объекта в мировых координатах:
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
+	// View matrix: размещение мира относительно камеры:
+	// Отодвинули мир вперёд (Z+ ось), т.е. отодвинули камеру назад.
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+	// Projection matrix: поправка объектов на перспективу и их клиппинг
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
+	
 	while (!glfwWindowShouldClose(win)) {
 		processInput(win);
 
 		glClearColor(background.r, background.g, background.b, background.a);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		model = glm::rotate(model, glm::radians(sin((float)glfwGetTime()) / 10), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(sin((float)glfwGetTime()) / 13), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(sin((float)glfwGetTime()) / 17), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		containerTexture->use();
 		basicShader->use();
 		basicShader->setInt("uDrawMode", drawMode);
-		basicShader->setFloatMat4("uTransform", glm::value_ptr(trans));
+		basicShader->setFloatMat4("model", glm::value_ptr(model));
+		basicShader->setFloatMat4("view", glm::value_ptr(view));
+		basicShader->setFloatMat4("projection", glm::value_ptr(projection));
 
 		glBindVertexArray(VAO_polygon);
-		glDrawElements(GL_TRIANGLES, numOfIndices * 3, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		// glDrawElements(GL_TRIANGLES, numOfIndices * 3, GL_UNSIGNED_INT, 0);
 
 
 
--- OpenGL_Tutorial/shaders/basic.vert
+++ OpenGL_Tutorial/shaders/basic.vert
@@ -7,11 +7,13 @@ layout (location = 2) in vec2 aTexCoord;
 out vec4 fColor;
 out vec2 fTexCoord;
 
-uniform mat4 uTransform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-	gl_Position = uTransform * vec4(aPos, 1.0f);
+	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 	fColor = aColor;
 	fTexCoord = aTexCoord;
 }
